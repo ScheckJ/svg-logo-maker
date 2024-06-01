cffab191141f13c98061f3363465a333c5d451cf
ScheckJ <scheckjah@yahoo.com>
2024-06-01 18:38:09 -0400

    created an index.js file and installed fs, jest and inquirer packages

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require ('inquirer')
+const {Circle, Square, Triangle} = require ('./lib/shapes')
+const fs = rewquire ('fs')
+
+class SVG{
+    constructor(){
+        this.textEl = ''
+        this.shapeEl = ''
+    }
+    
+}
