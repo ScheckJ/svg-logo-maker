10491e6e4ccef0d5218ce3bd2bd872960ed2bc45
ScheckJ <scheckjah@yahoo.com>
2024-08-04 16:11:05 -0400

    added questions to index.js

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const inquirer = require ('inquirer')
 const {Circle, Square, Triangle} = require ('./lib/shapes')
 const fs = rewquire ('fs')
+const Svg = require('./lib/svg')
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Enter three characters",
+    },
+    
+    {
+        type: "input",
+        name: "textColor",
+        message: "Enter text color:",
+    },
+    
+    {
+        type: "list",
+        name: "shape",
+        message: "Select a shape",
+        chocies: ["square", "triangle", "circle"]
+    },
+    
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Select shape color",
+    },
+    
+];
+
