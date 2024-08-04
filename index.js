39eeff368f46fd5f5f6b42b57440db0de016dddd
ScheckJ <scheckjah@yahoo.com>
2024-08-04 15:48:22 -0400

    added lib folder with shapes and tests files

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 const inquirer = require ('inquirer')
 const {Circle, Square, Triangle} = require ('./lib/shapes')
 const fs = rewquire ('fs')
-
-class SVG{
-    constructor(){
-        this.textEl = ''
-        this.shapeEl = ''
-    }
-    
-}
